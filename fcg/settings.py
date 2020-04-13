9abffc98d52edd947950a14b7de2bf6ac00374a5
Derth Adams <derth@me.com>
2020-04-12 19:29:16 -0700

    Modified settings to do override import of config at end

--- fcg/settings.py
+++ fcg/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .config import *
+# from .config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,12 +77,10 @@ WSGI_APPLICATION = 'fcg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'fcgdb',
-        'USER': db_user,
-        'PASSWORD': db_pass,
-        'HOST': 'localhost',
+        'NAME': 'db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'host',
         'PORT': 5432,
     }
 }
@@ -125,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .config import *
+except ImportError:
+    pass
