9731ea0346d8b93fe7f973d5e48706e135fe21b0
Derth Adams <derth@me.com>
2020-04-26 15:40:10 -0700

    Add API to INSTALLED_APPS

--- fcg/settings.py
+++ fcg/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiCongig',
     'graph.apps.GraphConfig',
     'django.contrib.admin',
     'django.contrib.auth',
