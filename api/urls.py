122f4b8519d1e97af39cbd1d0eef00f1d5c66eb7
Derth Adams <derth@me.com>
2020-04-26 17:16:22 -0700

    Add URL paths for API views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    re_path(r'^neighbors/$', views.NeighborsAPIView.as_view(), name='neighbors'),
+    re_path(r'^list/$', views.ListAPIView.as_view(), name='list'),
+    re_path(r'^root/$', views.RootAPIView.as_view(), name='root')
 ]
