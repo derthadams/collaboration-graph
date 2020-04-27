9d654339d1212775cbfcda90374e6f3b35e52de5
Derth Adams <derth@me.com>
2020-04-26 17:20:05 -0700

    Split line for length

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^neighbors/$', views.NeighborsAPIView.as_view(), name='neighbors'),
+    re_path(r'^neighbors/$', views.NeighborsAPIView.as_view(),
+            name='neighbors'),
     re_path(r'^list/$', views.ListAPIView.as_view(), name='list'),
     re_path(r'^root/$', views.RootAPIView.as_view(), name='root')
 ]
