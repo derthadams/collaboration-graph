b7ba33dd483438e9f1274676c4fa9ddf5731aa81
Derth Adams <derth@me.com>
2020-05-18 20:54:01 -0700

    Comment out url for root API endpoint

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     re_path(r'^neighbors/$', views.NeighborsAPIView.as_view(),
             name='neighbors'),
     re_path(r'^list/$', views.ListAPIView.as_view(), name='list'),
-    re_path(r'^root/$', views.RootAPIView.as_view(), name='root')
+    # re_path(r'^root/$', views.RootAPIView.as_view(), name='root')
 ]
