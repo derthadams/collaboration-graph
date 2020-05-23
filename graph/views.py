2b2468c4e28f19a309b6cb28ee9b4742507bcd25
Derth Adams <derth@me.com>
2020-05-23 13:27:55 -0700

    Delete commented-out code in graph/views.py

--- graph/views.py
+++ graph/views.py
@@ -1,30 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
 
-
-# def index(request):
-    # template = loader.get_template('graph/index_demo.html')
-    # context = {}
-    # return HttpResponse("Hello, world. You're at the graph index.")
-    # return HttpResponse(template.render(context, request))
-    # return render(request, 'graph/index_demo.html')
-
-# def index(request):
-#     name_list = []
-#     neo_driver = open_neo4j_session()
-#     with neo_driver.session() as session:
-#         results = session.read_transaction(get_crew_list, 'Person')
-#         for result in results:
-#             name_list.append(result['p.fullName'])
-#         for name in name_list:
-#             print(name)
-#     session.close()
-#     template = loader.get_template('graph/index_demo.html')
-#     context = {
-#         'name_list': name_list,
-#     }
-#     return HttpResponse(template.render(context, request))
 
 def index(request):
     template = loader.get_template('graph/index.html')
