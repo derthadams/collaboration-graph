84f90f83f2b4bd50e269a86292c9d8e67340f478
Derth Adams <derth@me.com>
2020-05-23 13:27:16 -0700

    Delete commented-out code in api/views.py

--- api/views.py
+++ api/views.py
@@ -1,16 +1,11 @@
 from django.views.generic import View
-from django.shortcuts import render
 from django.http import JsonResponse
 from .cypher import *
-
-# Create your views here.
 
 
 class NeighborsAPIView(View):
     def get(self, request, *args, **kwargs):
         uuid = self.request.GET.get('uuid')
-        # parent_uuid = self.request.GET.get('parent_uuid')
-        # elements = parse_neighbor_results(uuid, parent_uuid)
         elements = parse_neighbor_results(uuid)
         return JsonResponse(elements)
 
@@ -20,10 +15,3 @@ class ListAPIView(View):
         term = self.request.GET.get('term')
         name_list = parse_name_list(term)
         return JsonResponse(name_list)
-
-
-# class RootAPIView(View):
-#     def get(self, request, *args, **kwargs):
-#         uuid = self.request.GET.get('uuid')
-#         elements = parse_initial_node(uuid)
-#         return JsonResponse(elements)
