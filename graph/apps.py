d3c0bb577a62761049baeef7aed2c18318bbfa21
Derth Adams <derth@me.com>
2020-06-02 12:02:07 -0700

    Add comment header

--- graph/apps.py
+++ graph/apps.py
@@ -1,3 +1,12 @@
+"""
+Derth Adams
+CS 406
+June 5, 2020
+
+File name:      api/apps.py
+Description:    Creates the GraphConfig class, which represents the Graph app
+                and its configuration.
+"""
 from django.apps import AppConfig
 
 
