0a69320b95263a62789a35a748eed3a552d63569
Derth Adams <derth@me.com>
2020-06-02 12:01:20 -0700

    Add comment header

--- api/apps.py
+++ api/apps.py
@@ -1,3 +1,12 @@
+"""
+Derth Adams
+CS 406
+June 5, 2020
+
+File name:      api/apps.py
+Description:    Creates the ApiConfig class, which represents the api app
+                and its configuration.
+"""
 from django.apps import AppConfig
 
 
