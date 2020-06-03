32397732563294c4d97368be5f2641bb1b69006c
Derth Adams <derth@me.com>
2020-06-02 17:09:34 -0700

    Add comment header, function comments, delete unused code.

--- graph/static/graph/graph.js
+++ graph/static/graph/graph.js
@@ -1,9 +1,21 @@
+/*******************************************************************************
+* Derth Adams
+* CS 406
+* June 5, 2020
+*
+* File name:        graph.js
+* Description:      Contains all the logic for the client-side graph app
+*******************************************************************************/
 import { cyPrefs, layoutPrefs } from "./cy_config.js";
 
+// cy is the cytoscape graph object which uses the div id=graph as a container
 const cy = cytoscape(cyPrefs);
 const graph = document.getElementById('graph');
+
+// graphLayout is the force-directed layout object attached to cy
 let graphLayout = cy.layout(layoutPrefs);
 
+// Select interface elements and assign to variables for later use
 const searchBox = document.getElementById('search-input');
 const searchForm = document.getElementById('search-form');
 const expandAllBtn = document.getElementById('expand-all');
@@ -12,7 +24,17 @@ const infoPanel = document.getElementById('info-panel');
 const nameHeading = document.getElementById('name-heading');
 const jobHeading = document.getElementById('job-heading');
 const jobList = document.getElementById('job-list');
+
+// Constant which represents the maximum number of nodes for which the
+// "Expand all" button is active
 const MAX_NODES_EXPAND_ALL = 5;
+
+/*******************************************************************************
+* Function name:    refreshGraph()
+* Description:      Redraws the graph and runs the force-directed layout.
+*                   Used to update the graph after nodes are added.
+* Receives/Returns: None
+*******************************************************************************/
 
 function refreshGraph() {
     cy.maxZoom(1);
@@ -21,15 +43,33 @@ function refreshGraph() {
     graph.classList.add('visible');
 }
 
+/*******************************************************************************
+* Function name:    selectEdges(nodeID)
+* Description:      Selects all edges incident on a given node.
+* Receives:         nodeID  uuid of the cytoscape node object
+*******************************************************************************/
+
 function selectEdges(nodeID) {
-    let edges = cy.elements('edge[source = "' + nodeID + '"], edge[target = "' + nodeID + '"]');
+    let edges = cy.elements('edge[source = "' + nodeID + '"], ' +
+        'edge[target = "' + nodeID + '"]');
     edges.addClass('selected-edge');
 }
+
+/*******************************************************************************
+* Function name:    unselectEdges()
+* Description:      Unselects all selected edges.
+*******************************************************************************/
 
 function unselectEdges() {
     let selected_edges = cy.$('.selected-edge');
     selected_edges.removeClass('selected-edge');
 }
+
+/*******************************************************************************
+* Function name:    makeNodeSelected(node)
+* Description:      Takes a cytoscape node object and makes it selected.
+* Receives:         node    A cytoscape node object
+*******************************************************************************/
 
 function makeNodeSelected(node) {
     let selected = cy.$('node:selected');
@@ -38,6 +78,14 @@ function makeNodeSelected(node) {
     unselectEdges();
     selectEdges(node.id());
 }
+
+/*******************************************************************************
+* Function name:    activateInfoPanel(node)
+* Description:      Deletes the previously-active info panel and creates and
+*                   displays a new info panel for the node passed as an
+*                   argument.
+* Receives:         node    A cytoscape node object
+*******************************************************************************/
 
 function activateInfoPanel(node) {
     nameHeading.innerText = node.data('full_name');
@@ -54,15 +102,33 @@ function activateInfoPanel(node) {
     infoPanel.classList.add('visible');
 }
 
+/*******************************************************************************
+* Function name:    enableButtons()
+* Description:      Enables both the "Clear graph" and "Expand all" buttons
+*******************************************************************************/
+
 function enableButtons() {
     clearGraphBtn.removeAttribute('disabled');
     expandAllBtn.removeAttribute('disabled');
 }
 
+/*******************************************************************************
+* Function name:    disableButtons()
+* Description:      Disables both the "Clear graph" and "Expand all" buttons
+*******************************************************************************/
+
 function disableButtons() {
     clearGraphBtn.setAttribute('disabled', 'true');
     expandAllBtn.setAttribute('disabled', 'true');
 }
+
+/*******************************************************************************
+* Function name:    addNode(nodeJSON)
+* Description:      Takes a JSON object representing a node and passes is to
+*                   the cy object, which creates a node object inside the
+*                   graph model.
+* Receives:         nodeJSON    A JSON object representing node data
+*******************************************************************************/
 
 function addNode(nodeJSON) {
     let node = cy.getElementById(nodeJSON.data.id);
@@ -81,11 +147,23 @@ function addNode(nodeJSON) {
     }
 }
 
+/*******************************************************************************
+* Function name:    clearGraph()
+* Description:      Deletes all nodes from the graph, disables buttons,
+*                   and hides the info panel.
+*******************************************************************************/
+
 function clearGraph() {
     cy.nodes().remove();
     disableButtons();
     infoPanel.classList.remove('visible');
 }
+
+/*******************************************************************************
+* Function name:    expandAll()
+* Description:      Expands the first neighbors of all nodes on the graph
+*                   by calling expandNode() on each one.
+*******************************************************************************/
 
 function expandAll() {
     for(let node of cy.nodes()) {
@@ -95,6 +173,15 @@ function expandAll() {
     }
     refreshGraph();
 }
+
+/*******************************************************************************
+* Function name:    expandNode(ele)
+* Description:      Expands the first neighbors of the node passed as an
+*                   argument. Makes a GET request to the API endpoint
+*                   /api/neighbors/ and adds the resulting collection of
+*                   nodes to cy.
+* Receives:         ele     A cytoscape node object
+*******************************************************************************/
 
 function expandNode(ele) {
     fetch('/api/neighbors/?uuid=' + ele.data('id'))
@@ -109,6 +196,7 @@ function expandNode(ele) {
         });
 }
 
+// Preferences for the Context Menu attached to the cytoscape object
 const cxtMenuPrefs = {
     selector: 'node',
     menuRadius: 100,
@@ -121,16 +209,15 @@ const cxtMenuPrefs = {
     fillColor: '#CCD1D1', // the background colour of the menu
     activeFillColor: '#99A3A4', // the colour used to indicate the selected command
 
+    // Contains html content for the menu options, as well as callback functions
+    // to execute when each one is selected.
     commands: [
         {
             content: '<span><img src="" id="collapse" alt="Collapse" style="width:100%"></span>',
+            // Selects and deletes the children of the selected node
             select: function(ele){
                 ele.data('expanded', 'false');
-                // const children = cy.elements('node[par = "' + ele.id() + '"][expanded = "false"], edge[source = "' + ele.id() + '"]');
                 const children = cy.elements('node[par = "' + ele.id() + '"][expanded = "false"]');
-                for (let edge of cy.elements('edge[source = "' + ele.id() + '"]')) {
-                    console.log(edge.data('source'), edge.data('target'));
-                }
                 cy.remove(children);
                 refreshGraph();
             }
@@ -138,6 +225,7 @@ const cxtMenuPrefs = {
 
         {
             content: '<span><img src="" id="delete" alt="Delete" style="width:100%"></span>',
+            // Deletes the selected node
             select: function(ele){
                 cy.remove(ele);
                 infoPanel.classList.remove('visible');
@@ -147,6 +235,7 @@ const cxtMenuPrefs = {
 
         {
             content: '<span><img src="" id="expand" alt="Expand" style="width:100%"></span>',
+            // Expands the first children of the selected node
             select: function(ele) {
                 expandNode(ele);
             }
@@ -154,28 +243,44 @@ const cxtMenuPrefs = {
     ]
 };
 
+// Attach the Context Menu to cy using the context menu preferences
 cy.cxtmenu(cxtMenuPrefs);
 
+// Once all DOM content is loaded, initialize cy by running loadCy with no
+// elements.
 window.addEventListener('DOMContentLoaded', function() {
     let elements = [];
     loadCy(elements);
 });
 
+// Attaches 'submit' event listener to the search form which will prevent the
+// page from reloading if the user hits enter.
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
 });
 
+// Attaches 'click' event listener to the "Clear graph" button that triggers
+// clearGraph().
 clearGraphBtn.addEventListener('click', () => {
     clearGraph();
 });
 
+// Attaches 'click' event listener to the "Expand all" button that triggers
+// expandAll().
 expandAllBtn.addEventListener('click', () => {
     expandAll();
 });
 
+/*******************************************************************************
+* Selects the search-input element and converts it to a JQuery Autocomplete
+* widget, with an object containing preferences for the widget.
+*******************************************************************************/
 $("#search-input").autocomplete({
-    delay: 10,
-    minLength: 3,
+    delay: 10,      // Delay 10 ms before making GET request with term
+    minLength: 3,   // User must type in at least 3 characters
+
+    // Sets the source of the suggestion list data as the result of a GET
+    // request to the endpoint /api/list/ with the search term.
     source: function(request, response) {
         fetch('/api/list/?term=' + request.term)
             .then(res => res.json())
@@ -183,13 +288,19 @@ $("#search-input").autocomplete({
                 response(data.name_list);
             })
     },
+    // Prevents the search box from filling in with the uuid of the result
+    // the user has focused on.
     focus: function (event, ui) {
         event.preventDefault();
     },
+    // When the user selects a search result, callback function creates an
+    // object from that search result's data and adds it to the graph
+    // with addNode().
     select: function(event, ui) {
+        // Prevents the search box from filling in with the uuid of the
+        // selected result
         event.preventDefault();
         searchBox.value = '';
-        // console.log(ui);
         const node = {
             group: 'nodes',
             data: {
@@ -205,24 +316,42 @@ $("#search-input").autocomplete({
     }
 });
 
+/*******************************************************************************
+* Function name:    loadCy(elements)
+* Description:      Takes an object representing nodes and edges and adds it
+*                   to cy (the cytoscape object). Attaches a series of event
+*                   listeners to objects inside cy.
+* Receives:         elements    An object representing nodes and edges
+*******************************************************************************/
+
 function loadCy(elements) {
     cy.add(elements);
     refreshGraph();
 
-    cy.on('layoutstop', function() {
-        cy.fit();
-        cy.maxZoom(2);
-    });
-
+    /***************************************************************************
+    * Event:    tapstart
+    * Target:   node
+    * Callback: Waits for the user to hold down on the node for 510ms (until the
+    *           context menu appears), then adds the icons to the three
+    *           context menu options.
+    ***************************************************************************/
     cy.on('tapstart', 'node', function() {
-        // alert("tapstart");
         setTimeout(function() {
-            document.getElementById('collapse').src = img_url + "node_collapse_sm.png";
-            document.getElementById('delete').src = img_url + "delete_sm.png";
-            document.getElementById('expand').src = img_url + "expanded_sm.png";
+            document.getElementById('collapse').src = img_url
+                + "node_collapse_sm.png";
+            document.getElementById('delete').src = img_url
+                + "delete_sm.png";
+            document.getElementById('expand').src = img_url
+                + "expanded_sm.png";
         }, 510);
     });
 
+    /***************************************************************************
+    * Event:    tap
+    * Target:   Background of the graph visualization
+    * Callback: Disables the info panel and unselects any selected edges if the
+    *           user clicks/taps on the background.
+    ***************************************************************************/
     cy.on('tap', function(evt) {
         if (evt.target === cy)
         {
@@ -231,6 +360,13 @@ function loadCy(elements) {
         }
     });
 
+    /***************************************************************************
+    * Event:    tap
+    * Target:   Node
+    * Callback: Unselects all edges, selects edges incident on the node
+    *           that was tapped, activates info panel for node that was
+    *           tapped.
+    ***************************************************************************/
     cy.on('tap', 'node', function(evt){
         let node = evt.target;
         // console.log('You just clicked ' + node.id());
@@ -240,7 +376,16 @@ function loadCy(elements) {
         activateInfoPanel(node);
     });
 
+    /***************************************************************************
+    * Event:    layoutstop
+    * Target:   cy.layout
+    * Callback: Resizes layout to fit window, checks size of graph to
+    *           determine which buttons should be active, and then activates
+    *           or deactivates the necessary buttons.
+    ***************************************************************************/
     cy.on('layoutstop', function() {
+        cy.fit();
+        cy.maxZoom(2);
         if(!cy.nodes().size()) {
             disableButtons();
         }
