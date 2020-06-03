e201a28d620f8c8649c07c26e02767c444132a6d
Derth Adams <derth@me.com>
2020-06-02 17:23:35 -0700

    Add comment header

--- graph/urls.py
+++ graph/urls.py
@@ -1,3 +1,15 @@
+"""
+Derth Adams
+CS 406
+June 5, 2020
+
+File name:      graph/urls.py
+Description:    Sets urlpatterns, which is a list of URL regex patterns along
+                with the corresponding View functions that should be invoked
+                when the user enters a URL matching that pattern.
+
+                Routes the root URL to the function index().
+"""
 from django.urls import path
 from . import views
 
