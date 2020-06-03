cf8699841d6b9bf13532f0af65bb9d35b57a00c3
Derth Adams <derth@me.com>
2020-06-02 17:26:58 -0700

    Add comment header and function comments

--- graph/views.py
+++ graph/views.py
@@ -1,5 +1,20 @@
+"""
+Derth Adams
+CS 406
+June 5, 2020
+
+File name:      graph/views.py
+Description:    Contains the View function for the graph app
+"""
 from django.http import HttpResponse
 from django.template import loader
+
+"""
+Function name:      index(request)
+Description:        Takes in a GET request to the root URL of the web app,
+                    renders the HTML template for the Network View page,
+                    and sends it back as an HTTP response.
+"""
 
 
 def index(request):
